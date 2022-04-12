fba1475dc8fa5ed13ab7e806fdcebc56c4f1653a
Denis Ryndin <denbeeline@icloud.com>
2022-04-12 21:57:45 +0300

    app init

--- test_UI/ViewController.swift
+++ test_UI/ViewController.swift
@@ -1,19 +1,38 @@
-//
-//  ViewController.swift
-//  test_UI
-//
-//  Created by Денис Рындин on 12.04.2022.
-//
+import UIKit 
 
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class CounterViewController: UIViewController {
+    
+    var count = 0 {
+        didSet {
+            counterLabel.text = String(count)
+        }
     }
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var valueSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        counterLabel.textColor = .black
+        counterLabel.textAlignment = .center
+        counterLabel.text = "0"
+        addButton.setTitle("Add", for: .normal)
+        resetButton.setTitle("Reset", for: .normal)
+    }
 
+    @IBAction func add(_ sender: UIButton) {
+        count += 1
+
+    }
+    
+    @IBAction func setValue(_ sender: UISlider) {
+        count = Int(valueSlider.value)
+    }
+    @IBAction func reset(_ sender: UIButton) {
+        count = 0
+    }
 }
 
